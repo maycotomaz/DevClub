d17840a7036fa66952b80c05e9d332c6982b42d4
maycotomaz <mayco.tomaaz@gmail.com>
2024-01-26 15:27:39 -0300

    começando aprender JS

---
+++ JavaScript/tipos de dados/string.js
@@ -0,0 +1,1 @@
+var MyFistString = "minha string"
---
+++ JavaScript/variaveis/variaveis.js
@@ -0,0 +1,10 @@
+var nunber1 = 1 //nan deve usar i var
+let nunber2 = 2 //quando eu quiser alterar o valor com o tempo
+const nunber3 = 3 //nunca vai munda//
+//usar o snake case pra criar o let, e em ingles, tem que fazer sentido
+nunber1 = 2
+nunber2 = 10
+
+console.log(nunber1)
+console.log(nunber2)
+console.log(nunber3)
