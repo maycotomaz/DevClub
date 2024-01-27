7f3b692457d73f95c08b6827ee9d18376d7936e9
maycotomaz <mayco.tomaaz@gmail.com>
2024-01-27 08:07:26 -0300

    criei pasta tipos de dados e adicionei alguns

---
+++ JavaScript/tipos de dados/nunber.js
@@ -0,0 +1,5 @@
+ const NunberOne = 234
+ const NunberText = "234/2"
+
+ console.log(NunberOne)
+ console.log(NunberText)
--- JavaScript/tipos de dados/string.js
+++ JavaScript/tipos de dados/string.js
@@ -1,1 +1,10 @@
-var MyFistString = "minha string"
+let MyFistString = "minha string"
+let MySecondString = `masdsddsdsfd`
+
+let MyTextWithSuperPowers = `eu consigo pular linhas
+asdsadc
+assim `
+ let NunberOfPeopleClass = 34
+  
+ let MyText = `tinha exatamente ${NunberOfPeopleClass} nas aula`
+ console.log(MyText)
