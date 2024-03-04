3a13272feec3c831cf2a8386b177b766cf8250c1
maycotomaz <mayco.tomaaz@gmail.com>
2024-03-04 19:02:18 -0300

    Array

---
+++ JavaScript/tipos de dados/Array.js
@@ -0,0 +1,45 @@
+// vetor ou lista
+
+const Nunbers = [10, 5 , 100, 12]
+
+ const People = [{
+        name: "Mayco",
+        age: 22,
+        sex: "male",
+        height: 1.75,
+        address: {
+            street:"Jose Crispim",
+            Nunber: 186,
+            Bairro:"monte castelo",
+            Country:"brasil"
+    }
+},
+
+       {name: "vitoria",
+        age: 21,
+        sex: "female",
+        height: 1.55,
+        address: {
+            street:"Jose Crispim",
+            Nunber: 186,
+            Bairro:"monte castelo",
+            Country:"brasil"
+        }
+},
+
+    {   name: "Matheus",
+        age: 19,
+        sex: "male",
+        height: 1.73,
+        address: {
+            street:"Jose Crispim",
+            Nunber: 186,
+            Bairro:"monte castelo",
+            Country:"brasil"
+        }
+}
+
+]
+console.log(Nunbers)
+
+console.log(People)
