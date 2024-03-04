8bd8e099c0da4810ea143f14c3893108867124c1
maycotomaz <mayco.tomaaz@gmail.com>
2024-03-04 18:35:38 -0300

    objects

---
+++ JavaScript/tipos de dados/object.js
@@ -0,0 +1,18 @@
+
+
+
+
+const Person = {
+    name: "Mayco",
+    age: 22,
+    sex: "male",
+    height: 1.75,
+    address: {
+        street:"Jose Crispim",
+        Nunber: 186,
+        Bairro:"monte castelo",
+        Country:"brasil"
+    }
+
+}
+console.log(Person.address.Country)
