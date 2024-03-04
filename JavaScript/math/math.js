476c0f7e7b233c429bd5dff80a801d2ef7cd6231
maycotomaz <mayco.tomaaz@gmail.com>
2024-03-04 19:12:56 -0300

    math

---
+++ JavaScript/math/math.js
@@ -0,0 +1,8 @@
+
+
+console.log(Math.PI + 2)//mostra o valor de Pi
+console.log(Math.pow(2,2))//Permite usar a potencia
+console.log(Math.sqrt(9,3))//Raiz Quadrada
+console.log(Math.random())//mostra um numero aleatorio entre 0 e 1
+console.log(Math.ceil(23.1))//arendodar um numero para cima
+console.log(Math.floor(23,9))//arendodar um numero para baixo
