f7f2d7abca595ddc79e4bb3c1c3355d5314e9d9c
maycotomaz <mayco.tomaaz@gmail.com>
2024-03-19 21:41:35 -0300

    If e Else

---
+++ JavaScript/if-else/if-else.js
@@ -0,0 +1,14 @@
+/*
+if - se
+else - se nao
+*/
+
+const age =  17
+if(age >= 17){
+    console.log("maior de idade")
+}else{
+    console.log("nao e maior de idade")
+}
+
+
+console.log("terminou de executar meu codigo")
